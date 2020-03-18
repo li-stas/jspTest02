c725b3ba8dd432515effd50ca5c538d109373ed5
lista <lista@bk.ru>
2020-03-18 15:28:14 +0200

    LocalDateTime.parse(

--- src/main/java/app/Main90AddUpDate.java
+++ src/main/java/app/Main90AddUpDate.java
@@ -17,13 +17,13 @@ public class Main90AddUpDate {
         deptService.saveDept(dept);
 
         Emp emp9000 = new Emp(9000, "DAVID", "SALESMAN",7698,
-                LocalDateTime.parse("1991-09-29", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
+                LocalDateTime.parse("1991-09-29 00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
                 1550,1700);
         emp9000.setDept(dept);
         dept.addEmp(emp9000);
 
         Emp emp9001 = new Emp(9001, "OLGA", "SALESMAN",7698,
-                LocalDateTime.parse("2013-09-04",DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
+                LocalDateTime.parse("2013-09-04 00:00",DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
                 1999,999);
         emp9001.setDept(dept);
         dept.addEmp(emp9001);
--- src/main/java/app/Main90AddUpDateDele.java
+++ src/main/java/app/Main90AddUpDateDele.java
@@ -6,6 +6,8 @@ import app.services.DeptService;
 import app.services.DeptServiceImpl;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main90AddUpDateDele {
     public static void main(String[] args) throws SQLException {
@@ -14,11 +16,15 @@ public class Main90AddUpDateDele {
         Dept dept = new Dept(90,"Auto90","Sumy");
         deptService.saveDept(dept);
 
-        Emp emp9000 = new Emp(9000, "DAVID", "SALESMAN",7698, java.sql.Date.valueOf("1991-09-29"), 1550,1700);
+        Emp emp9000 = new Emp(9000, "DAVID", "SALESMAN",7698,
+                LocalDateTime.parse("1991-09-29", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
+                1550,1700);
         emp9000.setDept(dept);
         dept.addEmp(emp9000);
 
-        Emp emp9001 = new Emp(9001, "OLGA", "SALESMAN",7698, java.sql.Date.valueOf("2013-09-04"), 1999,999);
+        Emp emp9001 = new Emp(9001, "OLGA", "SALESMAN",7698,
+                LocalDateTime.parse("2013-09-04",DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
+                1999,999);
         emp9001.setDept(dept);
         dept.addEmp(emp9001);
         deptService.updateDept(dept);
